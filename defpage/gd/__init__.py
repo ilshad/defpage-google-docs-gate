de4606f14fa18b5462fce4551070cfa801e70775
Ilshad Khabibullin <astoon@spacta.com>
2012-02-13 15:59:42 +0400

    can be started

--- defpage/gd/__init__.py
+++ defpage/gd/__init__.py
@@ -4,19 +4,16 @@ from pyramid.exceptions import Forbidden
 from pyramid.httpexceptions import HTTPUnauthorized
 from pyramid.session import UnencryptedCookieSessionFactoryConfig
 from defpage.lib.authentication import UserInfoAuthenticationPolicy
-from defpage.lib.util import is_int
 from defpage.gd.config import system_params
-from defpage.gd.resources import get_root
 
 def main(global_config, **settings):
     system_params.update(settings)
-    session_factory = UnencryptedCookieSessionFactoryConfig("7oDVDSuJ")
+    session_factory = UnencryptedCookieSessionFactoryConfig("Gfj8bTsA")
     authentication_policy = UserInfoAuthenticationPolicy()
     config = Configurator()
     config.setup_registry(settings=settings,
                           session_factory=session_factory,
-                          authentication_policy=authentication_policy,
-                          root_factory=get_root)
+                          authentication_policy=authentication_policy)
 
     config.set_request_property("defpage.gd.security.get_user", "user", reify=True)
 
@@ -27,7 +24,6 @@ def main(global_config, **settings):
     config.add_view("defpage.gd.views.forbidden",
                     "", context=Forbidden,
                     renderer="defpage.gd:templates/unauthorized.pt")
-
     config.add_view("defpage.gd.views.unauthorized",
                     "", context=HTTPUnauthorized,
                     renderer="defpage.gd:templates/unauthorized.pt")
@@ -37,61 +33,5 @@ def main(global_config, **settings):
     config.add_view("defpage.gd.views.empty",
                     "error",
                     renderer="defpage.gd:templates/error.pt")
-    config.add_view("defpage.gd.views.default", "")
-
-    # collection
-    config.add_view("defpage.gd.views.create_collection",
-                    "create_collection",
-                    renderer="defpage.gd:templates/collection/create.pt",
-                    permission="create_collection")
-
-    config.add_route("display_collection",
-                     "/collection/{name}",
-                     custom_predicates=(is_int,))
-    config.add_view("defpage.gd.views.display_collection",
-                    route_name="display_collection",
-                    renderer="defpage.gd:templates/collection/display.pt")
-
-    config.add_route("collection_properties",
-                     "/collection/{name}/properties",
-                     custom_predicates=(is_int,))
-    config.add_view("defpage.gd.views.collection_properties",
-                    route_name="collection_properties",
-                    renderer="defpage.gd:templates/collection/properties.pt")
-
-    config.add_route("delete_collection",
-                     "/collection/{name}/delete",
-                     custom_predicates=(is_int,))
-    config.add_view("defpage.gd.views.delete_collection",
-                    route_name="delete_collection",
-                    renderer="defpage.gd:templates/collection/delete.pt")
-
-    config.add_route("collection_roles",
-                     "/collection/{name}/roles",
-                     custom_predicates=(is_int,))
-    config.add_view("defpage.gd.views.collection_roles",
-                    route_name="collection_roles",
-                    renderer="defpage.gd:templates/collection/roles.pt")
-
-    # source
-    config.add_route("source_overview",
-                     "/collection/{name}/source",
-                     custom_predicates=(is_int,))
-    config.add_view("defpage.gd.views.source_overview",
-                    route_name="source_overview",
-                    renderer="defpage.gd:templates/source/overview.pt")
-
-    # transmission
-    config.add_route("transmission_overview",
-                     "/collection/{name}/transmission",
-                     custom_predicates=(is_int,))
-    config.add_view("defpage.gd.views.transmission_overview",
-                    route_name="transmission_overview",
-                    renderer="defpage.gd:templates/transmission/overview.pt")
-
-    # public collections
-    config.add_view("defpage.gd.views.public_overview",
-                    "public",
-                    renderer="defpage.gd:templates/public/overview.pt")
 
     return config.make_wsgi_app()
--- defpage/gd/layout.py
+++ defpage/gd/layout.py
@@ -3,17 +3,8 @@ from defpage.gd.config import system_params
 
 def renderer_add_globals(e):
     e["layout"] = get_renderer("defpage.gd:templates/layout.pt").implementation()
+    e["base_url"] = system_params.base_url
     e["static_url"] = system_params.static_url
-    e["login_url"] = system_params.login_url
     e["logout_url"] = system_params.logout_url
-    e["accounts_url"] = system_params.accounts_url
     e["help_url"] = system_params.help_url
-    e["active_collection_id"] = active_collection(e["request"])
 
-def active_collection(req):
-    if req.path_info_pop() == "collection":
-        try:
-            return int(req.path_info_pop())
-        except:
-            None
-    return None
